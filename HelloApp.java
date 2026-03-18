f61029747e8ff50f3f205edd7c12a64bfe36027e
an4761 <an4761@srmist.edu.in>
2026-03-18 13:39:49 +0530

    UC4: Support multiple names

--- HelloApp.java
+++ HelloApp.java
@@ -1,21 +1,17 @@
 public class HelloApp {
     public static void main(String[] args) {
-
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello, World!");
-        }
-
-        // Default name
         String name = "World";
-
-        // Check if a name is provided as a command-line argument
         if (args.length > 0) {
-            name = args[0]; // Use the provided name
-        }
+            StringBuilder nameBuilder = new StringBuilder();
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
 
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+            name = nameBuilder.toString();
+        }
         System.out.println("Hello, " + name + "!");
     }
 }
